815af2f9588321affaf9f7ea590bb855282514ce
Mustapha Adams <adamsmustapha419@gmail.com>
2024-04-03 15:39:00 +0000

    Create task_one.dart

---
+++ on-boarding/task-1/task_one.dart
@@ -0,0 +1,102 @@
+class Task {
+  String? title, description, status;
+  DateTime? dueDate;
+
+  // constructor
+  Task(this.title, this.description, this.dueDate) : status = 'pending';
+
+  @override
+  String toString() {
+    return 'Title: $title\nDescription: $description\nDue Date: ${dueDate.toString()}\nStatus: $status\n';
+  }
+  
+}
+
+class TaskManager {
+
+  List<Task> tasks = [];
+  
+  void addTask(Task newTask) {
+    tasks.add(newTask);
+  }
+
+
+  void viewTasks() {
+    
+    for (var task in tasks) {
+      print(task);
+    }
+  }
+
+
+  void viewCompletedTasks() {
+
+    for (var task in tasks) {
+      if (task.status == 'completed') {
+        print(task);
+      }
+    }
+  }
+
+
+  void viewPendingTasks() {
+
+    for (var task in tasks) {
+      if (task.status == 'pending') {
+        print(task);
+      }
+    }
+    
+  }
+
+  
+  void editTask(int index, {String? title, description, status, DateTime? dueDate}) {
+    
+    if (index < 0 || index >= tasks.length) {
+      print('The task you are trying to edit does not exist.');
+      return;
+    }
+
+    var task = tasks[index];
+    task.title = title ?? task.title;
+    task.description = description ?? task.description;
+    task.dueDate = dueDate ?? task.dueDate;
+    task.status = status ?? task.status;
+  }
+
+
+  void deleteTask(int index) {
+    
+    if (index < 0 || index >= tasks.length) {
+      print('The task you are trying to delete does not exist.');
+      return;
+    }
+
+    tasks.removeAt(index);
+    print('Task deleted successfully.');
+
+  }
+}
+
+
+void main() {
+  // Testing instances and methods
+  TaskManager newTaskManager = TaskManager();
+  Task newTask1 = Task('Solve Leetcode', 'Solve one leetcode by close of today', DateTime(2024, 4, 3));
+  Task newTask2 = Task('Standup meeting', 'Meet the scrum tomorrow', DateTime(2024, 4, 4));
+  Task newTask3 = Task('Go hiking', 'Go hiking at Shai Hills ', DateTime(2024, 5, 23));
+
+  newTaskManager.addTask(newTask1);
+  newTaskManager.addTask(newTask2);
+  newTaskManager.addTask(newTask3);
+  newTaskManager.editTask(1, title: 'Updated Standup Meeting');
+  newTaskManager.editTask(0, status: 'completed');
+  newTaskManager.viewTasks();
+  newTaskManager.viewPendingTasks();
+  newTaskManager.viewCompletedTasks();
+  newTaskManager.deleteTask(7); 
+  newTaskManager.deleteTask(2); 
+  newTaskManager.viewTasks();
+}
+
+

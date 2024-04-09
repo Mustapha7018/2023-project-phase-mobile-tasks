2db5b48384d91dd8b309191632475b54749d26d6
Mustapha7018 <adamsmustapha419@gmail.com>
2024-04-09 11:58:32 +0000

    Add task app

---
+++ on-boarding/task_app/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'package:task_app/splash.dart';
+
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: SplashScreenView(),
+    ),
+  );
+}

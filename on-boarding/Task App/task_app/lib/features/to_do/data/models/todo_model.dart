05d8090708169bd001a0ddcb447fad2787f7c976
Mustapha7018 <adamsmustapha419@gmail.com>
2024-04-17 14:57:02 +0000

    add data models

--- on-boarding/Task App/task_app/lib/features/to_do/data/models/todo_model.dart
+++ on-boarding/Task App/task_app/lib/features/to_do/data/models/todo_model.dart
@@ -1,1 +1,20 @@
-import 'package:flutter/material.dart';
+import '../../domain/entities/add_task.dart';
+
+class TaskModel extends Task{
+
+  TaskModel({
+    required super.id,
+    required super.title,
+    required super.description,
+    required super.dueDate,
+  });
+
+
+  Map<String, dynamic> toJson() =>{
+    'id' : id,
+    'title' : title,
+    'description' : description,
+    'dueDate' : dueDate,
+  };
+  
+}
